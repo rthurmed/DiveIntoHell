afc30b5ffb64c4377b01057c15bff4a8bcb92ef7
Arthur Medeiros <rthurmedeiros@gmail.com>
2022-11-26 12:26:31 -0300

    Implemented arrows reflecting on walls

--- src/enemy/arrow/Arrow.gd
+++ src/enemy/arrow/Arrow.gd
@@ -1,25 +1,41 @@
-extends Area2D
+extends KinematicBody2D
 
 
 export var speed = 140
+export var speed_reflect_mutator = 1.15
+export var speed_mutation_min = 40
+export var speed_mutation_max = 300
 
 
 func _physics_process(delta):
-	position += Vector2.LEFT.rotated(rotation) * speed * delta
+	var movement = Vector2.LEFT.rotated(rotation) * speed
+	var collision = move_and_collide(movement * delta)
+	collide(collision, delta)
+
+
+func collide(collision: KinematicCollision2D, _delta):
+	if not collision:
+		return
+	
+	var body: Node = collision.collider
+	if body.has_method('damage'):
+		cause_damage(body)
+		return
+	
+	reflect(collision.normal)
+
+
+func cause_damage(body):
+	body.damage()
+	queue_free()
+
+
+func reflect(normal):
+	rotation = Vector2.LEFT.rotated(rotation).reflect(normal).angle()
+	speed = speed * speed_reflect_mutator
+	speed = clamp(speed, speed_mutation_min, speed_mutation_max)
 
 
 func _on_Timer_timeout():
 	# TODO: death animation?
 	queue_free()
-
-
-func _on_Arrow_body_entered(body):
-	if body.has_method('damage'):
-		body.damage()
-		queue_free()
-		return
-	
-	# bounce of walls and obstacles
-	# TODO
-	queue_free()
-	
